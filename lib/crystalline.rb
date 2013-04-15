d13a2c9963c88d7321b72190fe4f75d050e9d77e
Joe Fredette <jfredett@gmail.com>
2013-04-15 13:04:30 -0400

    implement README generator

--- lib/crystalline.rb
+++ lib/crystalline.rb
@@ -1,3 +1,3 @@
 require "crystalline/version"
 
-require 'pry'
+require 'tilt'
--- lib/crystalline/generators/readme.rb
+++ lib/crystalline/generators/readme.rb
@@ -1,3 +1,5 @@
+require 'tilt'
+
 module Crystalline
   module Generators
     class Readme
@@ -6,16 +8,43 @@ module Crystalline
         @name = name
       end
 
-      def template
-        @template ||= Tilt.new File.join('..', File.dirname(__FILE__), 'templates', 'README.md.erb')
+      def path
+        File.expand_path(
+          File.join(
+            File.dirname(File.expand_path(__FILE__)),
+            '..',
+            'templates',
+            'README.md.erb'
+          )
+        )
       end
 
-      def self.generate!(name)
-        new(name).generate!
+      def build_status_badge
+        "[![Build Status](https://travis-ci.org/#{@user}/#{@name}.png?branch=master)](http://travis-ci.org/#{@user}/#{@name})"
+      end
+
+      def code_climate_badge
+        "[![Code Climate](https://codeclimate.com/github/#{@user}/#{@name}.png)](https://codeclimate.com/github/#{@user}/#{@name})"
+      end
+
+      def coverage_status_badge
+        "[![Coverage Status](https://coveralls.io/repos/#{@user}/#{@name}/badge.png?branch=master)](https://coveralls.io/r/#{@user}/#{@name})"
+      end
+
+      def gem_version_badge
+        "[![Gem Version](https://badge.fury.io/rb/#{@name}.png)](http://badge.fury.io/rb/#{@name})"
+      end
+
+      def template
+        @template ||= Tilt.new path
+      end
+
+      def self.generate!(user, name)
+        new(user, name).generate!
       end
 
       def generate!
-        templates.render
+        template.render self
       end
     end
   end
